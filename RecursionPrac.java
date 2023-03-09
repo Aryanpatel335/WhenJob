07d33b8cc11b78c2a845ee1803918b1911f0e4c9
aryan <aryanp862002@gmail.com>
2023-03-08 21:24:19 -0500

    added changes to recurision

--- RecursionPrac.java
+++ RecursionPrac.java
@@ -5,14 +5,14 @@ public class RecursionPrac {
         return fibFunc(n - 1)  + fibFunc(n - 2);
     }
     public static int factorialfunc(int n){
-        if(n <= 0 ){
+        if(n <= 1 ){
             return 1;
         }
         return n * factorialfunc(n - 1);
     }
     public static void main(String[] args){
         System.out.println(factorialfunc(4));
-        System.out.println(fibFunc(9));
+        System.out.println(fibFunc(2));
     }
     
 }
