2c2518a00567c0cb7106bbef683973cd287f32d8
aryan <aryanp862002@gmail.com>
2023-04-02 13:39:21 -0400

    leetcode 56 merge intervals implementation

--- Medium/MergeIntervals.java
+++ Medium/MergeIntervals.java
@@ -2,5 +2,25 @@ package Medium;
 import java.util.*;
 
 public class MergeIntervals{
+
+    public int[][] merge(int[][] intervals){
+
+        //input: [[1,3],[2,6],[8,10],[15,18]]
+        //output: [[1,6],[8,10],[15,18]]
+
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+        LinkedList<int[]> merged = new LinkedList<>();
+
+        for(int[] curr : intervals){
+            if(merged.isEmpty() || merged.getLast()[1] < curr[0]){
+                merged.add(curr);
+            }
+            else{
+                merged.getLast()[1] = Math.max(merged.getLast()[1], curr[1]);
+        
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
     
 }
