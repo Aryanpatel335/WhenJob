5c51fd9bf69ef48bf9a0e1be9158248f958d6046
aryan <aryanp862002@gmail.com>
2023-04-03 23:26:47 -0400

    lc 974, wtf does prefix mod do

---
+++ Medium/SubarraySumsDivByK.java
@@ -0,0 +1,21 @@
+package Medium;
+
+public class SubarraySumsDivByK {
+    public int subarraysDivByK(int[] nums, int k){
+        int result = 0;
+        int prefixMod = 0;
+        
+        int[] modGroups = new int[k];
+
+        for(int num: nums){
+            prefixMod = (prefixMod + num % k + k) % k;
+
+            if(prefixMod % k == 0) result++;
+
+            result+= modGroups[prefixMod];  
+            modGroups[prefixMod]++;
+        }
+        return result;
+
+    }
+}
