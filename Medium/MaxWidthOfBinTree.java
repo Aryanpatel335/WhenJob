00947ef6252d7f49f76298fc56b073551b98fc00
aryan <aryanp862002@gmail.com>
2023-04-22 22:45:40 -0400

    incomplete lc 662

--- Medium/MaxWidthOfBinTree.java
+++ Medium/MaxWidthOfBinTree.java
@@ -1,5 +1,6 @@
 package Medium;
-
+import java.util.*;
+import javafx.util.Pair;
 class TreeNode{
     int val;
     TreeNode left;
@@ -21,6 +22,28 @@ class TreeNode{
 public class MaxWidthOfBinTree {
     
     public int MaxWidthOfBinTree(TreeNode root){
+        if(root == null){
+            return 0;
+        }
 
+        LinkedList<Pair<TreeNode,Integer>> queue = new LinkedList<>();
+        Integer maxWidth  = 0 ;
+        queue.add(new Pair<>(root,0));
+        while(queue.size() > 0){
+            Pair<TreeNode,Integer> head = queue.getFirst();
+            int size = queue.size();
+            Pair<TreeNode,Integer> elem = null;
+            for(int i = 0 ; i < size ; i++){
+                elem = queue.removeFirst();
+                if(elem.getKey().left != null){
+                    queue.add(new Pair<>(elem.getKey().left,2*elem.getValue()));
+                }
+                if(elem.getKey().right != null){
+                    queue.add(new Pair<>(elem.getKey().right,2*elem.getValue()+1));
+                }
+            }
+            maxWidth = Math.max(maxWidth, elem.getValue() - head.getValue - 1);
+        } 
+        return maxWidth;
     }
 }
