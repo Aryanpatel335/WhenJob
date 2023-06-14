8936ad6ab92a9b68554930ec84f5c3784004e472
Aryanpatel335 <aryanp862002@gmail.com>
2023-06-14 16:37:49 -0400

    dfs implementation

---
+++ Graph Algo/DFS.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+public class DFS {
+    private LinkedList<Integer> adjLists[];
+    
+    private boolean visited[];
+
+    DFS(int vertices){
+        adjLists = new LinkedList[vertices];
+        visited = new boolean[vertices];
+        for(int i = 0; i < vertices; i++){
+            adjLists[i] = new LinkedList<Integer>();
+        }
+
+    }
+
+    void addEdge(int src, int dest){
+        adjLists[src].add(dest);
+    }
+
+    void DFSRecur(int vertex){
+        visited[vertex] = true;
+        System.out.println(vertex + " ");
+        Iterator<Integer> ite = adjLists[vertex].listIterator();
+        while(ite.hasNext()){
+            int adj = ite.next();
+            if(!visited[adj]){
+                DFSRecur(adj);
+            }
+        }
+    }
+
+}
